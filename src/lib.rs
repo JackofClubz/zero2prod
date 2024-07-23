07939f7fa014738b149b152bacff6937b48c464d
JackofClubz <lancetersou@gmail.com>
2024-07-23 14:10:21 +0100

    changes to include extractor and new tests for servers

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,12 @@ use std::net::TcpListener;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
 
-async fn health_check(_req: HttpRequest) -> impl Responder {
-HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+HttpResponse::Ok().finish()
+}
+
+async fn subscribe() -> HttpResponse{
+    HttpResponse::Ok().finish()
 }
 
 #[tokio::main]
@@ -21,6 +25,7 @@ pub fn run(listener:TcpListener) -> Result<Server, std::io::Error>{
     let server = HttpServer::new(||{
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
         })
         .listen(listener)?
         .run();
